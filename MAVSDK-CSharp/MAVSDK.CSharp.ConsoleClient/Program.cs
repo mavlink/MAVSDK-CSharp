e445ab34a74c8979d832d1f5afaf4a2407f9a898
Patryk Acewicz-Hepfner <patryk.acewicz@gmail.com>
2019-07-21 20:03:36 +0200

    Make Main() async

--- MAVSDK-CSharp/MAVSDK.CSharp.ConsoleClient/Program.cs
+++ MAVSDK-CSharp/MAVSDK.CSharp.ConsoleClient/Program.cs
@@ -1,11 +1,12 @@
-﻿using Grpc.Core;
+﻿using System.Threading.Tasks;
+using Grpc.Core;
 using Mavsdk.Rpc.Action;
 
 namespace MAVSDK.CSharp.ConsoleClient
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			var channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
 			var actionServiceClient = new ActionService.ActionServiceClient(channel);
