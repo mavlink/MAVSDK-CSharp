4d81fff924e9ca280cde5261289aa6ffc093b503
Patryk Acewicz-Hepfner <patryk.acewicz@gmail.com>
2019-07-21 16:23:56 +0200

    Console client + dummy arm code

---
+++ MAVSDK-CSharp/MAVSDK.CSharp.ConsoleClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using Mavsdk.Rpc.Action;
+
+namespace MAVSDK.CSharp.ConsoleClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var channel = new Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
+			var actionServiceClient = new ActionService.ActionServiceClient(channel);
+
+			actionServiceClient.Arm(new ArmRequest(), new CallOptions());
+		}
+	}
+}
