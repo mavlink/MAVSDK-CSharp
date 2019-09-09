6e4575dbb45af798b2955c1c560f8078d4c7d8b3
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-09-09 15:56:08 +0200

    Improve example program

--- MAVSDK-CSharp/MAVSDK-CSharp/ActionException.cs
+++ MAVSDK-CSharp/MAVSDK-CSharp/ActionException.cs
@@ -3,15 +3,15 @@ using Mavsdk.Rpc.Action;
 
 namespace MAVSDK_CSharp
 {
-	public class ActionException : Exception
-	{
-		public ActionResult.Types.Result Result { get; }
-		public string ResultStr { get; }
+    public class ActionException : Exception
+    {
+        public ActionResult.Types.Result Result { get; }
+        public string ResultStr { get; }
 
-		public ActionException(ActionResult.Types.Result result, string resultStr)
-		{
-			Result = result;
-			ResultStr = resultStr;
-		}
-	}
+        public ActionException(ActionResult.Types.Result result, string resultStr)
+        {
+            Result = result;
+            ResultStr = resultStr;
+        }
+    }
 }
