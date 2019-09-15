a84798b251d1cafb98871585e273cff7b813e8a6
Patryk Acewicz-Hepfner <patryk.acewicz@gmail.com>
2019-09-16 00:38:30 +0200

    Refactor sample program to use MavSystem

--- MAVSDK-CSharp/MAVSDK.CSharp.ConsoleClient/Program.cs
+++ MAVSDK-CSharp/MAVSDK.CSharp.ConsoleClient/Program.cs
@@ -2,16 +2,15 @@
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using MAVSDK_CSharp.Plugins;
-using Action = MAVSDK_CSharp.Plugins.Action;
-using Console = System.Console;
+using Grpc.Core;
+using MAVSDK_CSharp;
 
 namespace MAVSDK.CSharp.ConsoleClient
 {
     class Program
     {
         private const string Host = "localhost";
-        private const string Port = "50051";
+        private const int Port = 50051;
 
         static async Task Main()
         {
@@ -23,26 +22,25 @@ namespace MAVSDK.CSharp.ConsoleClient
              *     - emit an event only when the value changes
              *     - discard the altitudes lower than 0
              */
-            var telemetry = new Telemetry(Host, Port);
-            telemetry.Position()
+            var drone = new MavSystem(Host, Port);
+            drone.Telemetry.Position()
                 .Select(position => Math.Round(position.RelativeAltitudeM, 1))
                 .DistinctUntilChanged()
                 .Where(altitude => altitude >= 0)
                 .Subscribe(Observer.Create<double>(altitude => Console.WriteLine($"altitude: {altitude}"), _ => { }));
             
             // Print the takeoff altitude.
-            var action = new Action(Host, Port);
-            action.GetTakeoffAltitude()
+            drone.Action.GetTakeoffAltitude()
                 .Do(altitude => Console.WriteLine($"Takeoff altitude: {altitude}"))
                 .Subscribe();
             
             // Arm, takeoff, wait 5 seconds and land.
             var tcs = new TaskCompletionSource<bool>();
-            action.Arm()
-                .Concat(action.Takeoff())
+            drone.Action.Arm()
+                .Concat(drone.Action.Takeoff())
                 .Delay(TimeSpan.FromSeconds(5))
-                .Concat(action.Land())
-                .Subscribe(Observer.Create<Unit>(_ => { }, () => tcs.SetResult(true)));
+                .Concat(drone.Action.Land())
+                .Subscribe(Observer.Create<Unit>(_ => { }, onError: _ => tcs.SetResult(false), onCompleted: () => tcs.SetResult(true)));
 
             // Wait until the takeoff routine completes (which happens when the landing starts)
             await tcs.Task;
